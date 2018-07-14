4064ef4ce4a0c39a43e5e3dd8b1a3147ab53ba47
Neerhom <32304258+Neerhom@users.noreply.github.com>
2018-07-14 13:35:57 +0300

    support for custom face textures

--- COM3D2.FaceType.Managed/Hooks.cs
+++ COM3D2.FaceType.Managed/Hooks.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System.Collections.Generic;
 using System.IO;
 namespace COM3D2.FaceType.Managed
@@ -6,24 +6,27 @@ namespace COM3D2.FaceType.Managed
  public static   class Hooks
     {
 
-        public static void FaceTypeHook ( TBody self, ref List<TBodySkin> goSlot, ref string slotname, ref int matno,
+        public static void FaceTypeHook ( TBody self, ref string slotname, ref int matno,
             ref string prop_name, ref string filename, ref Dictionary<string, byte[]> dicModTexData, ref MaidParts.PARTS_COLOR f_ePartsColorId )
           {
             int num = (int)TBody.hashSlotName[slotname];
             TBodySkin tBodySkin = self.goSlot[num];
             string modelname = (Path.GetFileNameWithoutExtension(tBodySkin.m_strModelFileName));
-
+                      
             int start = modelname.IndexOf("facetype");
-            if (slotname == "head" && start!=-1 && modelname.Length > start+10)
+            if (slotname == "head" && start != -1 && filename.IndexOf("*")!=-1 )
             {
-                 
-                string type = modelname.Substring(start + 8, 3);
+                if (!GameUty.FileSystemMod.IsExistentFile(filename.Replace("*", tBodySkin.m_strModelFileName)))
+                {
+                    string type = modelname.Substring(start + 8, 3);
 
-                filename =  filename.Replace("*", "face" + type);
-                          
+                    filename = filename.Replace("*", "face" + type);
+
+
+                }
 
             }
+        
         }
-
     }
 }
